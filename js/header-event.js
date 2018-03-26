801c8dcf431bfb2bfa471de90f9499d0444f79a9
Hercules <ducduc08@gmail.com>
2018-03-26 22:52:59 +0700

    Add files via upload

--- js/header-event.js
+++ js/header-event.js
@@ -6,9 +6,11 @@ set_main_tab_active("home");
 $("header-home").onclick = function() {
 	set_main_tab_active("home");
 }
+/*
 $("header-idea").onclick = function() {
 	set_main_tab_active("idea");
 }
+*/
 $("header-product").onclick = function() {
 	set_main_tab_active("product");
 }
@@ -34,7 +36,7 @@ function set_main_tab_active(tabidname) {
 }
 function set_active_button(tabidname) {
 	$("header-home").classList.remove("header-btn-active");
-    $("header-idea").classList.remove("header-btn-active");
+    /*$("header-idea").classList.remove("header-btn-active");*/
     $("header-product").classList.remove("header-btn-active");
     $("header-connect").classList.remove("header-btn-active");
     $("header-control").classList.remove("header-btn-active");
